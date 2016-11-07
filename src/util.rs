ad66b7a33a221da829a1efcf7a7dac4a4470ab26
JJag <jakub.jagusztyn@gmail.com>
2016-11-07 23:52:04 +0100

    Concatenate two u8 to make u16

---
+++ src/util.rs
@@ -0,0 +1,17 @@
+pub fn concat(x: u8, y: u8) -> u16 {
+    let x16 = x as u16;
+    let y16 = y as u16;
+    (x16 << 8) | y16
+}
+
+mod test {
+    use super::concat;
+    #[test]
+    fn concat_test() {
+        assert_eq!(concat(0xAF, 0x30), 0xAF30);
+        assert_eq!(concat(0x00, 0x00), 0x0000);
+        assert_eq!(concat(0x00, 0xFF), 0x00FF);
+        assert_eq!(concat(0xFF, 0x00), 0xFF00);
+        assert_eq!(concat(0xFF, 0xFF), 0xFFFF);
+    }
+}
