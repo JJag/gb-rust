6cd8df0e1f3e358c041a1640f0ecbd012aa2d4cd
JJag <jakub.jagusztyn@gmail.com>
2018-06-22 16:31:54 +0200

    Rename according to naming style

--- src/timer.rs
+++ src/timer.rs
@@ -10,10 +10,10 @@ impl TimerControl {
 
     fn from_u8(n: u8) -> TimerControl {
         let clock_freq = match n & 0b11 {
-            0b00 => TacFrequency::Hz_4096,
-            0b01 => TacFrequency::Hz_262144,
-            0b10 => TacFrequency::Hz_65536,
-            0b11 => TacFrequency::Hz_16384,
+            0b00 => TacFrequency::Hz4096,
+            0b01 => TacFrequency::Hz262144,
+            0b10 => TacFrequency::Hz65536,
+            0b11 => TacFrequency::Hz16384,
         };
         let enabled = n & 0b100 != 0;
         TimerControl {
@@ -25,8 +25,8 @@ impl TimerControl {
 
 #[derive(FromPrimitive)]
 enum TacFrequency {
-    Hz_4096 = 0b00,
-    Hz_262144 = 0b01,
-    Hz_65536 = 0b10,
-    Hz_16384 = 0b11,
+    Hz4096 = 0b00,
+    Hz262144 = 0b01,
+    Hz65536 = 0b10,
+    Hz16384 = 0b11,
 }
