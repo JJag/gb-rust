a7dbc52f951ab4e537fb5dd071948ebafc443530
JJag <jakub.jagusztyn@gmail.com>
2018-08-09 20:00:14 +0200

    Split gb-related rendering logic and actual drawing with OpenGL by using Array2D as buffer

--- src/gfx.rs
+++ src/gfx.rs
@@ -5,6 +5,7 @@ use piston::event_loop::*;
 use piston::input::*;
 use piston::window::WindowSettings;
 use std::time::{Duration, Instant};
+use util::Array2D;
 use super::gpu::Color;
 
 #[derive(Copy, Clone)]
@@ -97,68 +98,87 @@ fn build_tilemap(vram: &[u8]) -> Tilemap {
 }
 
 
-fn draw_tile(c: &Context, gl: &mut GlGraphics, x_offset: i32, y_offset: i32, tile: &Tile) {
+fn draw_tile(x_offset: i32, y_offset: i32, tile: &Tile, out_buf: &mut Array2D) {
     for y in 0..8 {
         for x in 0..8 {
-            let color = 1f32 - tile.get_pixel(x, y) as f32 / 4f32;
-            let square = rectangle::square(1.0, 1.0, 1.0);
-            let global_x = x_offset + x;
-            let global_y = y_offset + y;
-            let transform = c.transform.trans(global_x as f64, global_y as f64);
-            let rgba = [color, color, color, 1f32];
-            rectangle(rgba, square, transform, gl);
+            let color_idx = tile.get_pixel(x, y);
+            let global_x = (x_offset + x);
+            let global_y = (y_offset + y);
+            if global_x >= 0 && global_x < out_buf.width() as i32 && global_y >= 0 && global_y < out_buf.height() as i32 {
+                out_buf.set(global_x as usize, global_y as usize, color_idx)
+            }
         }
     }
 }
 
 impl Gfx {
-    pub fn render_framebuffer(&mut self, args: &RenderArgs, framebuffer: &[Color]) {
-        self.gl.draw(args.viewport(), |c, gl| {
-            clear([0.3, 0.3, 0.3, 1.0], gl);
-            for y in 0..145 {
-                for x in 0..161 {
-                    let square = rectangle::square(1.0, 1.0, 1.0);
-                    let transform = c.transform.trans(x as f64, y as f64);
-                    let color = framebuffer[y * 144 + x];
-                    let rgba = to_rgba(color);
-                    rectangle(rgba, square, transform, gl);
-                }
-            }
-        });
+    pub fn build_framebuffer(&mut self, vram: &[u8], sc_x: u8, sc_y: u8) -> Array2D {
+        let sc_x = sc_x as f32;
+        let sc_y = sc_y as f32;
+        let first_tile_col_idx = (sc_x / 8.0) as u8;
+        let first_tile_row_idx = (sc_y / 8.0) as u8;
+        unimplemented!()
     }
 
+    //    pub fn render(&buf: )
+    pub fn render_framebuffer(&mut self, args: &RenderArgs, vram: &[u8], sc_x: u8, sc_y: u8) {
+        let buf = self.build_framebuffer(vram, sc_x, sc_y);
+        self.render_buf(args, &buf);
+    }
+
+    pub fn render_tileset(&mut self, args: &RenderArgs, vram: &[u8]) {
+        let buf = self.render_tileset_to_buf(vram);
+        self.render_buf(args, &buf);
+    }
 
     /// 24 rows - 16 tiles each
-    pub fn render_tileset(&mut self, args: &RenderArgs, vram: &[u8]) {
+    fn render_tileset_to_buf(&mut self, vram: &[u8]) -> Array2D {
         let tileset = build_tileset(vram);
-        self.gl.draw(args.viewport(), |c, gl| {
-            clear([0.3, 0.0, 0.0, 1.0], gl);
-
-            for tidx in 0..384 {
-                let row_num = tidx / 16;
-                let col_num = tidx % 16;
-                let x_offset = col_num * 9; // 9 is used for 1px spacing between each tile
-                let y_offset = row_num * 9; // 9 is used for 1px spacing between each tile
-
-                let tile = &tileset.tiles[tidx as usize];
-                draw_tile(&c, gl, x_offset, y_offset, tile);
-            }
-        });
+        let mut out_buf = Array2D::new(17 * 8, 25 * 8);
+        for tidx in 0..384 {
+            let row_num = tidx / 16;
+            let col_num = tidx % 16;
+            let x_offset = col_num * 9; // 9 is used for 1px spacing between each tile
+            let y_offset = row_num * 9; // 9 is used for 1px spacing between each tile
+            let tile = &tileset.tiles[tidx as usize];
+            draw_tile(x_offset, y_offset, tile, &mut out_buf);
+        }
+        out_buf
     }
 
-
     pub fn render_tilemap(&mut self, args: &RenderArgs, vram: &[u8], sc_x: u8, sc_y: u8) {
+        let buf = self.render_tilemap_to_buf(vram, sc_x, sc_y);
+        self.render_buf(args, &buf);
+    }
+    fn render_tilemap_to_buf(&mut self, vram: &[u8], sc_x: u8, sc_y: u8) -> Array2D {
+        let mut out_buf = Array2D::new(32 * 8, 32 * 8);
         let tileset = build_tileset(vram);
         let tilemap = build_tilemap(vram);
         use graphics::*;
+        for tile_x in 0..32 {
+            for tile_y in 0..32 {
+                let tile = tilemap.get(&tileset, 0, tile_x, tile_y);
+                let offset_x = tile_x * 8;
+                let offset_y = tile_y * 8;
+                draw_tile(offset_x, offset_y, tile, &mut out_buf);
+            }
+        }
+        out_buf
+    }
+
+
+    pub fn update(&mut self, _args: &UpdateArgs) {}
+
+    fn render_buf(&mut self, args: &RenderArgs, buf: &Array2D) {
         self.gl.draw(args.viewport(), |c, gl| {
             clear([0.3, 0.0, 0.0, 1.0], gl);
-            for tile_x in 0..32 {
-                for tile_y in 0..32 {
-                    let tile = tilemap.get(&tileset, 0, tile_x, tile_y);
-                    let offset_x = tile_x * 8;
-                    let offset_y = tile_y * 8;
-                    draw_tile(&c, gl, offset_x, offset_y, tile);
+            let square = rectangle::square(1.0, 1.0, 1.0);
+            for x in 0..buf.width() {
+                for y in 0..buf.height() {
+                    let color = 1f32 - buf.get(x, y) as f32 / 4f32;
+                    let transform = c.transform.trans(x as f64, y as f64);
+                    let rgba = [color, color, color, 1f32];
+                    rectangle(rgba, square, transform, gl);
                 }
             }
 
@@ -166,18 +186,8 @@ impl Gfx {
             let SCREEN_HEIGHT: f64 = 144.0;
             let square = rectangle::rectangle_by_corners(1.0, 1.0, 1.0 + SCREEN_WIDTH, 1.0 + SCREEN_HEIGHT);
             let red = [1.0, 0.0, 0.0, 0.3];
-            let sc_x = sc_x as f64;
-            let sc_y = sc_y as f64;
-            // 4 rectangles are drawn to achieve wrapping
-            rectangle(red, square, c.transform.trans(sc_x - SCREEN_WIDTH * 0.0, sc_y - SCREEN_HEIGHT * 0.0), gl);
-            rectangle(red, square, c.transform.trans(sc_x - SCREEN_WIDTH * 2.0, sc_y - SCREEN_HEIGHT * 0.0), gl);
-            rectangle(red, square, c.transform.trans(sc_x - SCREEN_WIDTH * 0.0, sc_y - SCREEN_HEIGHT * 2.0), gl);
-            rectangle(red, square, c.transform.trans(sc_x - SCREEN_WIDTH * 2.0, sc_y - SCREEN_HEIGHT * 2.0), gl);
         });
     }
-
-
-    pub fn update(&mut self, _args: &UpdateArgs) {}
 }
 
 fn to_rgba(c: Color) -> [f32; 4] {
