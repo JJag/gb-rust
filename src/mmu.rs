9c052c2390d6aa06f1f1f5a547f0a78e038435d7
JJag <jakub.jagusztyn@gmail.com>
2018-12-16 21:53:56 +0100

    Add flag for VRAM and OAM locking support

--- src/mmu.rs
+++ src/mmu.rs
@@ -7,7 +7,6 @@ use crate::timer::TimerControl;
 use crate::util;
 
 const VRAM_SIZE: usize = 8 * 1024;
-const EXT_RAM_SIZE: usize = 8 * 1024;
 const WORK_RAM_SIZE: usize = 8 * 1024;
 const OAM_SIZE: usize = 160;
 const IO_SIZE: usize = 128;
@@ -28,6 +27,8 @@ pub struct Mmu {
     pub timer: Timer,
     pub joypad: Joypad,
     pub ppu: Ppu,
+    dma_cycles_left: u32,
+    restrict_vram_oam: bool,
 }
 
 impl Mmu {
@@ -51,6 +52,8 @@ impl Mmu {
             timer: timer,
             joypad: joypad,
             ppu: ppu,
+            dma_cycles_left: 0,
+            restrict_vram_oam: false,
         };
 
         mmu
@@ -71,14 +74,26 @@ impl Mmu {
             match addr {
                 0x0000...0x3FFF => self.cart.read_byte(addr as u16),
                 0x4000...0x7FFF => self.cart.read_byte(addr as u16),
-                0x8000...0x9FFF => self.vram[addr - 0x8000],
+                0x8000...0x9FFF => {
+                    if !self.restrict_vram_oam || self.ppu.mode != GpuMode::VramAccess {
+                        self.vram[addr - 0x8000]
+                    } else { 0xFF }
+                },
                 0xA000...0xBFFF => self.cart.read_byte(addr as u16),
                 0xC000...0xDFFF => self.work_ram[addr - 0xC000],
                 0xE000...0xFDFF => self.work_ram[addr - 0xE000],
-                0xFE00...0xFE9F => self.oam[addr - 0xFE00],
+                0xFE00...0xFE9F => {
+                    let mode = self.ppu.mode;
+                    if !self.restrict_vram_oam || mode != GpuMode::VramAccess && mode != GpuMode::OamAccess {
+                        self.oam[addr - 0xFE00]
+                    } else { 0xFF }
+                },
 
                 0xFF00          => self.joypad.read_byte(),
-                0xFF04          => self.timer.div(),
+                0xFF04          => {
+                    eprintln!("self.timer.div() = {:?}", self.timer.div());
+                    self.timer.div()
+                },
                 0xFF05          => self.timer.tima(),
                 0xFF06          => self.timer.tma,
                 0xFF07          => self.timer.tac.to_u8(),
@@ -120,11 +135,20 @@ impl Mmu {
         match addr {
             0x0000...0x3FFF => self.cart.write_byte(addr as u16, val),
             0x4000...0x7FFF => self.cart.write_byte(addr as u16, val),
-            0x8000...0x9FFF => self.vram[addr - 0x8000] = val,
+            0x8000...0x9FFF => {
+                if !self.restrict_vram_oam || self.ppu.mode != GpuMode::VramAccess {
+                    self.vram[addr - 0x8000] = val
+                }
+            },
             0xA000...0xBFFF => self.cart.write_byte(addr as u16, val),
             0xC000...0xDFFF => self.work_ram[addr - 0xC000] = val,
             0xE000...0xFDFF => self.work_ram[addr - 0xE000] = val,
-            0xFE00...0xFE9F => self.oam[addr - 0xFE00] = val,
+            0xFE00...0xFE9F => {
+                let mode = self.ppu.mode;
+                if !self.restrict_vram_oam || mode != GpuMode::VramAccess && mode != GpuMode::OamAccess {
+                    self.oam[addr - 0xFE00] = val
+                }
+            },
             0xFF00          => self.joypad.write_byte(val),
             0xFF04          => self.timer.reset_div(),
             0xFF05          => self.timer.set_tima(val),
@@ -158,11 +182,8 @@ impl Mmu {
     fn dma(&mut self, src: u8) {
         assert!(src <= 0xF1);
         let src_from = (src as u16) << 8;
-        let dst_from = 0xFE00;
-
         for i in 0x00..0xA0 {
-            let b = self.read_byte(src_from + i);
-            self.write_byte(b, dst_from + i);
+            self.oam[i] = self.read_byte(src_from + i as u16);
         }
     }
 
